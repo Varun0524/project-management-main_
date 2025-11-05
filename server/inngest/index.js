02f0b5aeae6c5825cd653c3ef7336015c7de40c9
varun0824 <varunshukla185@gmail.com>
2025-11-05 13:25:31 +0530

    second commit

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,5 +1,5 @@
 import { Inngest } from "inngest";
-import prisma from "../config/prisma";
+import prisma from "../configs/prisma.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "project-management" });
@@ -13,7 +13,7 @@ const syncUserCreation = inngest.createFunction(
         await prisma.user.create({
             data:{
                 id:data.id,
-                email: data.email_addresses[0]?.email_addres,
+                email: data?.email_addresses[0]?.email_addres,
                 name: data?.first_name + " " + data?.last_name,
                 image: data?.image_url,
             }
@@ -35,7 +35,7 @@ const syncUserDeletion = inngest.createFunction(
     }
 )
 
-//Inngest Function to delete user data to a database
+//Inngest Function to update user data to a database
 const syncUserUpdation = inngest.createFunction(
     {id: 'sync-user-from-clerk'},
     {event: 'clerk/user.updated'},
@@ -46,7 +46,7 @@ const syncUserUpdation = inngest.createFunction(
                 id:data.id
             },
             data:{
-                email: data.email_addresses[0]?.email_addres,
+                email: data?.email_addresses[0]?.email_addres,
                 name: data?.first_name + " " + data?.last_name,
                 image: data?.image_url,
             }
