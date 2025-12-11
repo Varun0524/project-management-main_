f88991edf598a1a5c19205225db06f4dad9a80ac
Varun0524 <barunsh2002@gmail.com>
2025-12-11 14:10:52 +0530

    spelling corrected

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -120,7 +120,7 @@ const syncWorkspaceDeletion = inngest.createFunction(
 
 const syncWorkspaceMemberCreation = inngest.createFunction(
     { id: 'sync-workspace-member-from-clerk'},
-    { event: 'clerk/organiztaionInvitation.accepted'},
+    { event: 'clerk/organizationInvitation.accepted'},
     async ({event}) => {
         const { data } = event;
         await prisma.workspaceMember.create({
